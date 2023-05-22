2d21510e6b3e279d437954a2798974db0926d2f2
Vinay <vinaychougale9222@gmail.com>
2023-05-22 19:41:59 +0530

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int day = 30;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Project"),
+      ),
+      body: Container(
+        child: Center(
+          child: Text("Welcome to the $day new project"),
+        ),
+      ),
+      drawer: Drawer(
+        child: Text("New Project"),
+      ),
+    );
+  }
+}
